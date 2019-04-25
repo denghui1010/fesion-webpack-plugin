398c9d50b9b0bc9311da025fd442fbb33feeeafc
刘登辉 <9798291@qq.com>
2019-04-25 16:24:37 +0800

    转为json格式

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ function FesionWebpackPlugin() {}
 FesionWebpackPlugin.prototype.apply = function(compiler) {
     //我们主要关注 compilation 阶段，即 webpack 打包阶段
     compiler.plugin("emit", function(compilation, callback) {
-        var filelist = "In this build:\n\n";
+        const filelist = [];
         //compilation.assets 和 compilation.chunks 前面已经说过
         for (var filename in compilation.assets) {
-            filelist += "- " + filename + "\n";
+            filelist.push({ path: filename });
         }
+        const fileStr = JSON.stringify(filelist);
         //在 compilation.assets 中添加需要的资源
         compilation.assets["filelist.md"] = {
             source: function() {
-                return filelist;
+                return fileStr;
             },
             size: function() {
-                return filelist.length;
+                return fileStr.length;
             }
         };
         callback();
